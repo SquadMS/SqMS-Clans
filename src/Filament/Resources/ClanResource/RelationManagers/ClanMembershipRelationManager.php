27dacc73a4924f9fd43d2ac604ab14eb02da330e
Patrick <privat@skyraptor.eu>
2022-06-12 15:45:06 +0200

    Create ClanMembershipRelationManager.php

---
+++ src/Filament/Resources/ClanResource/RelationManagers/ClanMembershipRelationManager.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SquadMS\AdminConfig\Filament\Resources\AdminConfigResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\RelationManagers\HasManyRelationManager;
+use Filament\Resources\Table;
+use Filament\Tables;
+
+class ClanMembershipRelationManager extends HasManyRelationManager
+{
+    protected static string $relationship = 'members';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            Forms\Components\BelongsToSelect::make('user_id')
+                ->relationship('user', 'name')
+                ->required(),
+            Forms\Components\Toggle::make('admin')
+                ->required(),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('user.avatar'),
+                Tables\Columns\TextColumn::make('user.name')->sortable(),
+                Tables\Columns\TextColumn::make('user.steam_id_64')->sortable(),
+                Tables\Columns\BooleanColumn::make('clan_membership.admin')->sortable(),
+            ])
+            ->filters([
+                //
+            ]);
+    }
+}

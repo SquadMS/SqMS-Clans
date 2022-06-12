6a552c2d649d46deec5cad1043eeabe7ef2fd028
Patrick <privat@skyraptor.eu>
2022-06-12 15:42:28 +0200

    Create ListClan.php

---
+++ src/Filament/Resources/ClanResource/Pages/ListClan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SquadMS\Clans\Filament\Resources\ClanResource\Pages;
+
+use SquadMS\Clans\Filament\Resources\ClanResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListClan extends ListRecords
+{
+    protected static string $resource = ClanResource::class;
+}

0adabf4980068bfeebd9fae207eb5738fb0db857
Patrick <privat@skyraptor.eu>
2022-06-12 15:18:27 +0200

    Create SquadMSModule.php

---
+++ src/SquadMSModule.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SquadMS\Clans;
+
+use Illuminate\Console\Scheduling\Schedule;
+use SquadMS\Foundation\Modularity\Contracts\SquadMSModule as SquadMSModuleContract;
+
+class SquadMSModule extends SquadMSModuleContract
+{
+    public static function getIdentifier(): string
+    {
+        return 'sqms-clans';
+    }
+
+    public static function getName(): string
+    {
+        return 'SquadMS Clans';
+    }
+
+    public static function publishAssets(): void
+    {
+        //
+    }
+
+    public static function registerAdminMenus(): void
+    {
+        //
+    }
+
+    public static function registerMenuEntries(string $menu): void
+    {
+        //
+    }
+
+    public static function schedule(Schedule $schedule): void
+    {
+        //
+    }
+}

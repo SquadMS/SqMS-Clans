6f576a46dfd5495e6bee017bf965f0a57f0d5f44
Patrick <privat@skyraptor.eu>
2022-06-12 15:41:11 +0200

    Create CreateClan.php

---
+++ src/Filament/Resources/ClanResource/Pages/CreateClan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SquadMS\Clans\Filament\Resources\ClanResource\Pages;
+
+use SquadMS\Clans\Filament\Resources\ClanResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateClan extends CreateRecord
+{   
+    protected static string $resource = ClanResource::class;
+}

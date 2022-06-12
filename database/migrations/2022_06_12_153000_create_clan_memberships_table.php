827b6b73d6330a5c64c2b38587597bc4f83185d0
Patrick <privat@skyraptor.eu>
2022-06-12 15:30:13 +0200

    Update and rename 2022_06_12_153000_create_clan_memberships_table to 2022_06_12_153000_create_clan_memberships_table.php

---
+++ database/migrations/2022_06_12_153000_create_clan_memberships_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->nullOnDelete();
+            
+            $table->boolean('admin');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clans');
+    }
+}

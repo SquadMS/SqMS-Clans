391504e72ee6eb25f198d0f1c4cbe651844eb4f9
Patrick <privat@skyraptor.eu>
2022-06-12 15:41:51 +0200

    Create EditClan.php

---
+++ src/Filament/Resources/ClanResource/Pages/EditClan.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SquadMS\Clans\Filament\Resources\ClanResource\Pages;
+
+use SquadMS\Clans\Filament\Resources\ClanResource;
+use Filament\Pages\Actions\Action;
+use Filament\Resources\Pages\EditRecord;
+
+class EditClan extends EditRecord
+{
+    protected static string $resource = ClanResource::class;
+}

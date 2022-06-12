7db28cfddf3198b9419ab5212d11a4ca738eb93e
Patrick <privat@skyraptor.eu>
2022-06-12 15:19:07 +0200

    Create ClansServiceProvider.php

---
+++ src/ClansServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SquadMS\Clans;
+
+use Illuminate\Support\ServiceProvider;
+
+class ClansServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        /* Configuration */
+        $this->mergeConfigFrom(__DIR__.'/../config/sqms-clans.php', 'sqms-clans');
+        
+        /* Load Migrations */
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+}

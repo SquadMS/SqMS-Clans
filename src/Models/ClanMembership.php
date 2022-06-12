e4fdb8328068ada09530ff5868eac9e34cdcb5b8
Patrick <privat@skyraptor.eu>
2022-06-12 15:36:59 +0200

    Create ClanMembership.php

---
+++ src/Models/ClanMembership.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SquadMS\Clans\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use SquadMS\Foundation\Models\SquadMSUser;
+
+class ClanMembership extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'admin'
+    ];
+    
+    function clan() : BelongsTo
+    {
+        return $this->belongsTo(Clan::class);
+    }
+    
+    function user() : BelongsTo
+    {
+        return $this->belongsTo(SquadMSUser::class);
+    }
+}

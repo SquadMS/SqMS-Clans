a711a7974c64387b9f64c4ce3b4a7ee390fd6652
Patrick <privat@skyraptor.eu>
2022-08-07 12:11:55 +0200

    Add clan tag column

--- database/migrations/2022_06_12_150000_create_clans_table.php
+++ database/migrations/2022_06_12_150000_create_clans_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->bigIncrements('id');
 
             $table->string('name');
+            $table->string('tag');
 
             $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->nullOnDelete();
--- src/Models/Clan.php
+++ src/Models/Clan.php
@@ -14,6 +14,7 @@ class Clan extends Model
      */
     protected $fillable = [
         'name',
+        'tag',
     ];
 
     public function memberships(): HasMany
